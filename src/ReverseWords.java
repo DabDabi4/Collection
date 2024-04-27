93653e9a52768ac0775c82524633b916f3e7e366
DabDabi4 <c.mashkarynets.artur@student.uzhnu.edu.ua>
2024-04-27 15:55:25 +0300

    Fist

--- src/ReverseWords.java
+++ src/ReverseWords.java
@@ -1,16 +1,13 @@
 public class ReverseWords {
     public static String reverseWords(String s) {
-        // Розділяємо вхідний рядок на слова
         String[] words = s.split(" ");
 
-        // Проходимося по кожному слову і перевертаємо його, якщо воно має 5 або більше символів
         for (int i = 0; i < words.length; i++) {
             if (words[i].length() >= 5) {
                 words[i] = reverseString(words[i]);
             }
         }
 
-        // Збираємо слова назад у рядок з пробілами
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < words.length; i++) {
             sb.append(words[i]);
@@ -22,7 +19,6 @@ public class ReverseWords {
         return sb.toString();
     }
 
-    // Функція для перевертання рядка
     public static String reverseString(String s) {
         return new StringBuilder(s).reverse().toString();
     }
